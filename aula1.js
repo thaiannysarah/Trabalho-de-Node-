f8f10e322711285b8bd04542162ab77df317fba9
Thaianny Moraes <115707413+thaiannysarah@users.noreply.github.com>
2022-11-25 10:31:52 -0300

    Update aula1.js

--- aula1.js
+++ aula1.js
@@ -1,6 +1,8 @@
 var http = require('http');
 http.createServer(function (req,res){
-    res.write('Brasil 2 x 0 Sevria \n');
+   res.writeHead(200, {'content-type': 'text/html'});
+    res.write('<h2>Brasil 2 x 0 Sevria</h2> \n');
+    res.write(Date().substring(16,24)+ '<br>');
     res.end('Thaianny linda!');
     
 }).listen(3000);
