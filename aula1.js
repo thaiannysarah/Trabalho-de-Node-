077ec57868143e1300f4cd085b9f00e84feb661f
Thaianny Moraes <115707413+thaiannysarah@users.noreply.github.com>
2022-11-25 10:49:40 -0300

    Update aula1.js
    placar

--- aula1.js
+++ aula1.js
@@ -1,8 +1,10 @@
 var http = require('http');
+var dia= require('./biblioteca');
 http.createServer(function (req,res){
    res.writeHead(200, {'content-type': 'text/html'});
-    res.write('<h2>Brasil 2 x 0 Sevria</h2> \n');
+    res.write('<h2>Brasil 2 x 1 </h2> \n');
     res.write(Date().substring(16,24)+ '<br>');
-    res.end('Thaianny linda!');
+    res.write('<p style=color:pink>'+Date().substring(16,24)+'<br>');
+    res.end('Thaianny Linda');
     
 }).listen(3000);
