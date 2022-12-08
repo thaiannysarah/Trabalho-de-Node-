a21a9e7b52347ac338c40f20e815fb37dc3e413a
Thaianny Moraes <115707413+thaiannysarah@users.noreply.github.com>
2022-12-08 10:39:46 -0300

    Delete server.js

--- server.js
+++
@@ -1,9 +0,0 @@
-var http = require('http');
-var fs = require('fs');
-http.createServer(function (req,res){
-fs.readFile('index.html', function(err, data) {
-    res.writeHead(200, {'content-type': 'text/htlm'});
-    res.write(data);
-    return res.end();
-})
-}).listen(80);
