d02e5305c17c20ba0540f299762e3ce01c249643
Thaianny Moraes <115707413+thaiannysarah@users.noreply.github.com>
2022-12-08 10:40:08 -0300

    Delete criararquivo.js

--- criararquivo.js
+++
@@ -1,6 +0,0 @@
-var fs= require ('fs');
-
-fs.appendFile('novoArquivo.text', 'Vai aparecer dentro do arquivo...', function (err){
-    if (err) throw err;
-    console.log('Arquivo criado...');
-});
